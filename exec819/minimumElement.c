21e868be77fc4e29406760bae6da7972dc388aa7
Marcos Souza <marcos.mass@hotmail.com>
2016-07-26 07:54:26 -0300

    Melhorei o caso para middle == 0
    para nao acontecer de acessar elem[-1]

--- exec819/minimumElement.c
+++ exec819/minimumElement.c
@@ -11,15 +11,19 @@ int main()
 	int begin = 0;
 	int end;
 
-    scanf("%d %d",&t, &n);
-	end = n-1;
+//quantidade de testes
+    scanf("%d",&t);
+	end = n-1; //elemento final do vetor
+
+  //carregando elementos
     for(z = 0; z < t; z++){
+      scanf("%d", &n);
         for(i = 0; i < n; i++){
             scanf("%d", &elements[i]);
         }
 
+//funcao que procura o inicio do vetor circular (o menor valor)
 		searchMin(elements, 0, n - 1);
-		scanf("%d", &n);
 	}
     return 0;
 }
@@ -29,11 +33,21 @@ void searchMin( int elem[SIZE], int begin, int end){
 
    //printf("begin = %d, middle = %d, end = %d, elem[midle] = %d\n", begin, middle, end, elem[middle]);
 
-    if(elem[middle - 1] > elem[middle] || begin >= end)
+   //caso do menor acontecer na primeira posicao
+   if(middle > 0) //se for maior que zero verifique o elemento em middle - 1
+   {
+     if(elem[middle - 1] > elem[middle])
+     {
+       printf("%d\n", elem[middle]); //é o primeiro
+       return;
+     }
+   }
+
+    if(begin >= end) //no final da recursao so resta o menor elemento
     {
         printf("%d\n", elem[middle]);
         return;
-    }
+    } //chamada recursiva T(n/2)
  	 else if(elem[begin] > elem[end] && elem[middle] > elem[end])
     {
      searchMin(elem, middle + 1, end);
